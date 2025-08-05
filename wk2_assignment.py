f6a1422f8e6a1c52fd7e93fe348700bb1f0d3ac5
teonea19 <teonea@yahoo.com>
2025-08-05 21:42:27 +0100

    Wk2 submission

---
+++ wk2_assignment.py
@@ -0,0 +1,45 @@
+#1 create my_list
+my_list = []
+
+
+#2 append to my_list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+
+print(my_list)
+
+#3 insert to my_list
+my_list.insert(1, 15)
+
+print(my_list)
+
+#create another list
+my_list1 = [50, 60, 70]
+
+print(my_list1)
+
+#4 extend my_list1 to my_list
+my_list.extend(my_list1)
+
+print(my_list)
+
+#5 remove last element from my_list
+my_list.remove(70)
+
+print(my_list)
+
+#6 sort my_list in ascending order
+my_list.sort()
+
+print(my_list)
+
+#7 find and print the index of value 30 in my_list
+print(my_list[3])
+
+
+
+
+
